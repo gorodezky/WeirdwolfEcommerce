b906678da438458d07deaa6cf8a6a9ccdb42202b
Dan Gorodezky <dan.gorodezky@yale.edu>
2017-06-30 20:52:45 -0700

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+theBear = Product.create(title: "Bear (on white)",
+	subtitle: "Weirdwolf Product", image_name: "bribear.jpg",
+	price: "20.00", sku: "bear1", download_url: "https://s3-us-west-1.amazonaws.com/rails-weirdecommerce-bucket/bribear.jpg",
+	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Bear down</p>}
+	)
