6c16edd20a997b6560a7f5a5bdeba4478064d880
Dan Gorodezky <dan.gorodezky@yale.edu>
2017-07-08 18:39:50 -0700

    changed product descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
-theSkull = Product.create(title: "Skull", 
- subtitle: "Weirdwolf Product", image_name: "skull.jpg",
- price: "18.00", sku: "skullOne", info: "Freshly Printed", download_url: "https://s3-us-west-2.amazonaws.com/rails-learnapp-bucket/kitty.JPG", 
- details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>A modern pop</p>
+theSkull = Product.create(title: "The Skull T", 
+ subtitle: "", image_name: "skull.jpg",
+ price: "22.00", sku: "skullOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+ details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Skull Tee. Complimentary U.S. shipping.</p>
  })
 
-theBear = Product.create(title: "Bear", 
-	subtitle: "Weirdwolf Product", image_name: "bear.jpg", 
-	price: "18.00", sku: "bearOne", info: "Freshly Printed", download_url: "https://s3-us-west-1.amazonaws.com/rails-weirdecommerce-bucket/bribear.jpg", 
-	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Bear down</p>})
+theBear = Product.create(title: "The Bear T", 
+	subtitle: "", image_name: "bear.jpg", 
+	price: "22.00", sku: "bearOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Bear Tee. Complimentary U.S. shipping.</p>})
 
-theWolf = Product.create(title: "Wolf", 
-	subtitle: "Weirdwolf Product", image_name: "blackwolf.jpg", 
-	price: "18.00", sku: "wolfOne", info: "Freshly Printed", download_url: "https://s3-us-west-1.amazonaws.com/rails-weirdecommerce-bucket/ww.jpg", 
-	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>The Original</p>})
+theWolf = Product.create(title: "The Weirdwolf T", 
+	subtitle: "", image_name: "blackwolf.jpg", 
+	price: "22.00", sku: "wolfOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Original Tee. Complimentary U.S. shipping.</p>})
 
-theMelon = Product.create(title: "Watermelon", 
-	subtitle: "Weirdwolf Product", image_name: "watermelon.jpg", 
-	price: "18.00", sku: "watermelonOne", info: "Freshly Printed", download_url: "https://s3-us-west-1.amazonaws.com/rails-weirdecommerce-bucket/ww.jpg", 
-	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Picked daily</p>})
+theMelon = Product.create(title: "The Watermelon T", 
+	subtitle: "", image_name: "watermelon.jpg", 
+	price: "22.00", sku: "watermelonOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Watermelon Tee. Complimentary U.S. shipping.</p>})
 
-theZebra = Product.create(title: "Zebra", 
-	subtitle: "Weirdwolf Product", image_name: "zebra.jpg", 
-	price: "18.00", sku: "zebraOne", info: "Freshly Printed", download_url: "https://s3-us-west-1.amazonaws.com/rails-weirdecommerce-bucket/ww.jpg", 
-	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>More colors than a rainbow</p>})
+theZebra = Product.create(title: "The Zebra T", 
+	subtitle: "", image_name: "zebra.jpg", 
+	price: "22.00", sku: "zebraOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Zebra Tee. Complimentary U.S. shipping.</p>})
