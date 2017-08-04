9a5d0d959f0e0efbcdddcf262d399db69e39461c
Dan Gorodezky <dan.gorodezky@yale.edu>
2017-08-04 15:03:15 -0700

    deleted product, added price to info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'weirdwolf', password_confirmation: 'weirdwolf')
+AdminUser.create!(email: 'dangorodezky@gmail.com', password: 'weirdwolf', password_confirmation: 'weirdwolf')
+AdminUser.create!(email: 'weirdwolfdesigns@gmail.com', password: 'weirdwolf', password_confirmation: 'weirdwolf')
 
-theSkull = Product.create(title: "The Skull T", 
- subtitle: "", image_name: "skull.jpg",
- price: "22.00", sku: "skullOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
- details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Skull Tee. Complimentary U.S. shipping.</p>
- })
-
-theBear = Product.create(title: "The Bear T", 
+theBear = Product.create(title: "The Bear", 
 	subtitle: "", image_name: "bear.jpg", 
-	price: "22.00", sku: "bearOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	price: "22.00", sku: "bearOne", info: "* $22 * 100 Percent Cotton T-Shirt", download_url: "", 
 	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Bear Tee. Complimentary U.S. shipping.</p>})
 
-theWolf = Product.create(title: "The Weirdwolf T", 
+theWolf = Product.create(title: "The Weirdwolf", 
 	subtitle: "", image_name: "blackwolf.jpg", 
-	price: "22.00", sku: "wolfOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	price: "22.00", sku: "wolfOne", info: "* $22 * 100 Percent Cotton T-Shirt", download_url: "", 
 	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Original Tee. Complimentary U.S. shipping.</p>})
 
-theMelon = Product.create(title: "The Watermelon T", 
+theMelon = Product.create(title: "The Watermelon", 
 	subtitle: "", image_name: "watermelon.jpg", 
-	price: "22.00", sku: "watermelonOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	price: "22.00", sku: "watermelonOne", info: "* $22 * 100 Percent Cotton T-Shirt", download_url: "", 
 	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Watermelon Tee. Complimentary U.S. shipping.</p>})
 
-theZebra = Product.create(title: "The Zebra T", 
+theZebra = Product.create(title: "The Zebra", 
 	subtitle: "", image_name: "zebra.jpg", 
-	price: "22.00", sku: "zebraOne", info: "* 100 Percent Cotton T-Shirt", download_url: "", 
+	price: "22.00", sku: "zebraOne", info: "* $22 * 100 Percent Cotton T-Shirt", download_url: "", 
 	details: "Digitaly Printed onto a Bella Canvas Tee", description: %{<p>Weirdwolf Design's Zebra Tee. Complimentary U.S. shipping.</p>})
